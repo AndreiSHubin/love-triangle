0b3e9f4b6ce10e1b4d4d9fe41a44c80fbf2f633a
AnreiSHubin <andrei_shubin_84@rambler.ru>
2018-09-22 22:40:03 +0300

    task 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 /**
- * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
+ * @param preferences - an array of integers. Indices of people, whom they love
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+ var i;
+ var counter = 0;
+ for ( i = 0; i < preferences.length; i++){
+  a = preferences[i]-1;
+  b = preferences[a]-1;
+  c = preferences[b]-1;
+  if (c == i && a >= 0 && b >=0 && c >=0 &&
+    a != b &&
+    b != c &&
+    c != a){
+ 
+    counter++
+  }
+ 
+ }
+return counter/3;
+};  
